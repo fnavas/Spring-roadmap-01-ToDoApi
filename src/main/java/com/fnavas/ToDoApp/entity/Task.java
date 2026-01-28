4e9e49d919c57d4d4f0c39295064352d91c890e9
fnavas <fnavas.dev@gmail.com>
2026-01-28 11:33:06 +0100

    feat: Add Task entity and initial data setup in application properties and data.sql

---
+++ src/main/java/com/fnavas/ToDoApp/entity/Task.java
@@ -0,0 +1,23 @@
+package com.fnavas.ToDoApp.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 100)
+    private String title;
+    private String description;
+    @Column(nullable = false)
+    private Boolean completed = Boolean.FALSE;
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDate created;
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO task (title, description, completed, created)
+VALUES ('Task 1', 'Description for Task 1', FALSE, CURRENT_DATE);
+
