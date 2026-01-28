2f37a83b7864738f1cc3908375b26172807c1f02
fnavas <fnavas.dev@gmail.com>
2026-01-28 12:52:50 +0100

    refactor: Add additional sample tasks to data.sql for testing purposes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,11 @@
 INSERT INTO task (title, description, completed, created)
 VALUES ('Task 1', 'Description for Task 1', FALSE, CURRENT_DATE);
+INSERT INTO task (title, description, completed, created)
+VALUES ('Task 2', 'Description for Task 2', FALSE, CURRENT_DATE);
+INSERT INTO task (title, description, completed, created)
+VALUES ('Task 3', 'Description for Task 3', FALSE, CURRENT_DATE);
+INSERT INTO task (title, description, completed, created)
+VALUES ('Task 4', 'Description for Task 4', FALSE, CURRENT_DATE);
+INSERT INTO task (title, description, completed, created)
+VALUES ('Task 5', 'Description for Task 5', FALSE, CURRENT_DATE);
 
