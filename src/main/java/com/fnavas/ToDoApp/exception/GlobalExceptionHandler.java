23503806fe79cdabd8d091ce6a0515a08165c55c
fnavas <fnavas.dev@gmail.com>
2026-01-29 10:10:51 +0100

    feat: Enhance TaskServiceImpl with exception handling for task retrieval and updates

--- src/main/java/com/fnavas/ToDoApp/exception/GlobalExceptionHandler.java
+++ src/main/java/com/fnavas/ToDoApp/exception/GlobalExceptionHandler.java
@@ -1,14 +1,17 @@
 package com.fnavas.ToDoApp.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler(TaskNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleException(TaskNotFoundException e) {
+    public ResponseEntity<ErrorResponse> TaskNotFoundHandleException(TaskNotFoundException e) {
+        log.info("[TaskNotFoundHandleException]-"+ e.getMessage());
         ErrorResponse errorResponse = new ErrorResponse(
                 404,
                 "Task not found",
