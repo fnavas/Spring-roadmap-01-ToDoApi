bd82b30786f1f9ca1093a7a2d3431c8320dc30f5
fnavas <fnavas.dev@gmail.com>
2026-01-29 10:33:00 +0100

    feat: Add global exception handling for generic exceptions in GlobalExceptionHandler

--- src/main/java/com/fnavas/ToDoApp/exception/GlobalExceptionHandler.java
+++ src/main/java/com/fnavas/ToDoApp/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
@@ -11,12 +12,21 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
     @ExceptionHandler(TaskNotFoundException.class)
     public ResponseEntity<ErrorResponse> TaskNotFoundHandleException(TaskNotFoundException e) {
-        log.info("[TaskNotFoundHandleException]-"+ e.getMessage());
+        log.info("[TaskNotFoundHandleException]- {}", e.getMessage());
         ErrorResponse errorResponse = new ErrorResponse(
                 404,
                 "Task not found",
                 e.getMessage()
         );
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+    public ResponseEntity<ErrorResponse> ExceptionHandleException(Exception e) {
+        log.info("[ExceptionHandleException]- {}", e.getMessage());
+        ErrorResponse errorResponse =  new ErrorResponse(
+                500,
+                "Internal Server Error",
+                e.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
     }
 }
