8248b4932a74b0a2da793728c26d3c7f40913511
fnavas <fnavas.dev@gmail.com>
2026-02-07 09:20:38 +0100

    feat: Add custom query methods to TaskRepository for case-insensitive title and description search

--- src/main/java/com/fnavas/ToDoApi/repository/TaskRepository.java
+++ src/main/java/com/fnavas/ToDoApi/repository/TaskRepository.java
@@ -2,9 +2,14 @@ package com.fnavas.ToDoApi.repository;
 
 import com.fnavas.ToDoApi.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
     public List<Task> findByCompleted(Boolean completed);
+    @Query("SELECT t FROM Task t WHERE LOWER(t.title) LIKE LOWER(CONCAT('%', :title, '%'))")
+    public List<Task> findByTitleContainingIgnoreCase(String title);
+    @Query("SELECT t FROM Task t WHERE LOWER(t.description) LIKE LOWER(CONCAT('%', :description, '%'))")
+    public List<Task> findByDescriptionContainingIgnoreCase(String description);
 }
