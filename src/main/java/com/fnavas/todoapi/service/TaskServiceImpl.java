ec598bc6b7cce0c4a090cefbba83682550b561a6
fnavas <fnavas.dev@gmail.com>
2026-03-24 17:33:34 +0100

    refactor: Simplify logging in TaskServiceImpl by removing redundant messages and enhancing clarity

--- src/main/java/com/fnavas/todoapi/service/TaskServiceImpl.java
+++ src/main/java/com/fnavas/todoapi/service/TaskServiceImpl.java
@@ -25,8 +25,7 @@ public class TaskServiceImpl implements TaskService {
 
     @Override
     public TaskDto findById(Long id) {
-        log.info("[findById]-Finding task by id");
-        log.debug("[findById]-Finding task by id {}", id);
+        log.info("[findById] id={}", id);
         Task task = taskRepository.findById(id).orElseThrow(
                 () -> new TaskNotFoundException("Task with id " + id + " not found"));
         return taskMapper.toDto(task);
@@ -34,30 +33,28 @@ public class TaskServiceImpl implements TaskService {
 
     @Override
     public Page<TaskDto> findAll(TaskFilter filter) {
-        log.info("[findAll]-Finding all tasks");
+        log.info("[findAll] page={}, size={}, sortBy={}, sortDir={}", filter.getPage(), filter.getSize(), filter.getSortBy(), filter.getSortDir());
         Specification<Task> spec = Specification.where((root, query, cb) -> cb.conjunction());
         if (filter.getTitle() != null && !filter.getTitle().isBlank()) {
-            log.info("[findAll]-Finding all tasks with title containing ignore case");
-            log.debug("[findAll]-Finding all tasks with title containing ignore case {}", filter.getTitle());
+            log.info("[findAll] filter title={}", filter.getTitle());
             spec = spec.and((root, query, criteriaBuilder) ->
                     criteriaBuilder.like(criteriaBuilder.lower(root.get("title")), "%" + filter.getTitle().toLowerCase() + "%"));
         }
 
         if (filter.getDescription() != null && !filter.getDescription().isBlank()) {
-            log.info("[findAll]-Finding all tasks with description containing ignore case");
-            log.debug("[findAll]-Finding all tasks with description containing ignore case {}", filter.getDescription());
+            log.info("[findAll] filter description={}", filter.getDescription());
             spec = spec.and((root, query, criteriaBuilder) ->
                     criteriaBuilder.like(criteriaBuilder.lower(root.get("description")), "%" + filter.getDescription().toLowerCase() + "%"));
         }
 
         if (filter.getCompleted() != null) {
-            log.info("[findAll]-Finding all tasks with completed {}", filter.getCompleted());
+            log.info("[findAll] filter completed={}", filter.getCompleted());
             spec = spec.and((root, query, criteriaBuilder) ->
                     criteriaBuilder.equal(root.get("completed"), filter.getCompleted()));
         }
 
         if (filter.getPriority() != null) {
-            log.info("[findAll]-Finding all tasks with priority {}", filter.getPriority());
+            log.info("[findAll] filter priority={}", filter.getPriority());
             spec = spec.and((root, query, criteriaBuilder) ->
                     criteriaBuilder.equal(root.get("priority"), filter.getPriority()));
         }
@@ -72,16 +69,14 @@ public class TaskServiceImpl implements TaskService {
 
     @Override
     public TaskDto createTask(TaskDto taskDto) {
-        log.info("[createTask]-Creating task");
-        log.debug("[createTask]-Creating task {}", taskDto);
+        log.info("[createTask] title={}", taskDto.title());
         Task task = taskMapper.toEntity(taskDto);
         return taskMapper.toDto(taskRepository.save(task));
     }
 
     @Override
     public TaskDto updateTaskById(Long id, TaskDto taskDto) {
-        log.info("[updateTaskById]-Finding task by id");
-        log.debug("[updateTaskById]-Finding task by id {}", id);
+        log.info("[updateTaskById] id={}", id);
         Task task = taskRepository.findById(id).orElseThrow(
                 () -> new TaskNotFoundException("Task with id " + id + " not found"));
         task.setTitle(taskDto.title());
@@ -89,19 +84,14 @@ public class TaskServiceImpl implements TaskService {
         task.setCompleted(taskDto.completed() != null ? taskDto.completed() : task.getCompleted());
         task.setPriority(taskDto.priority() != null ? taskDto.priority() : task.getPriority());
         task.setDueDate(taskDto.dueDate());
-        log.info("[updateTaskById]-Updating task by id");
-        log.debug("[updateTaskById]-Updating task by id {}", id);
         return taskMapper.toDto(taskRepository.save(task));
     }
 
     @Override
     public void deleteTaskById(Long id) {
-        log.info("[deleteTaskById]-Finding task by id");
-        log.debug("[deleteTaskById]-Finding task by id {}", id);
+        log.info("[deleteTaskById] id={}", id);
         taskRepository.findById(id).orElseThrow(
                 () -> new TaskNotFoundException("Task with id " + id + " not found"));
-        log.info("[deleteTaskById]-Deleting task by id");
-        log.debug("[deleteTaskById]-Deleting task by id {}", id);
         taskRepository.deleteById(id);
     }
 }
